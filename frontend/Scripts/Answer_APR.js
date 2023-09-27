9f18ec72ac41dcc556412483399b793b57ea4a5e
Victor Junior da Silva Ramos <victorjr.muchacho@gmail.com>
2023-09-27 05:15:07 -0300

    add link do servidor railway

--- frontend/Scripts/Answer_APR.js
+++ frontend/Scripts/Answer_APR.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://quiz-cmb-teste-production.up.railway.app';
 const socket = io(BASE_URL);
 
 const ROOMID = localStorage.getItem("roomId");
